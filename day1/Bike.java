1c537b6ccd4b801e785616deb741674d72db658c
pavisen <76270482+pavisen@users.noreply.github.com>
2023-10-29 16:37:44 +0530

    Add files via upload
    day1

---
+++ day1/Bike.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Bike {
+	
+	public static void main(String[]args)
+	{
+		Car toyota = new Car();
+		Bike unicorn = new Bike();
+		
+		toyota.applyBrake();
+		toyota.soundHorn();
+	}
+
+}
---
+++ day1/BrowserChrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class BrowserChrome {
+	
+
+		
+		public static void main(String[] args ) {
+			
+			Chrome obj = new Chrome();
+			
+			obj.getName();
+			obj.printName();
+		}
+
+}
+
